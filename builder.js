e3404372a4a534f06ad816bf5cb5db9feed78737
Lewis Nyman <hello@lewisnyman.co.uk>
2013-02-19 22:24:23 +0000

    Added styling, demo content, and iframe resizing

--- builder.js
+++ builder.js
@@ -4,15 +4,35 @@ $.ajax({
   // type of data we are expecting in return:
   dataType: 'text',
   success: function(data){
-    console.log(data);
     var doc = jsyaml.load(data);
-    console.log(doc);
-    var page   = $("#page").html();
     var template = Handlebars.compile(page);
     var html = template(doc);
     $('body').html(html);
+    $('iframe').iframeAutoHeight();
   },
   error: function(xhr, type){
     alert('Ajax error!')
   }
 })
+
+var title = "<title>{{title}} | {{subtitle}}</title>";
+
+var page = 
+"<div class='page'>" +
+"<h1>{{title}}</h1>" +
+"<h2>{{subtitle}}</h2>" +
+"<p>{{introduction}}</p>" +
+"<h2>Colors</h2>" +
+"<ul>" +
+"{{#each colors}}" +
+"  <li>{{name}} - #{{hex}}</li>" +
+"{{/each}}" +
+"</ul>" +
+"<h2>Components</h2>" +
+"{{#each components}}" +
+"  <section>" +
+"    <h3>{{title}}</h3>" +
+"    <iframe src='{{url}}'></iframe>" +
+"  <section>" +
+"{{/each}}" +
+"</div>";
